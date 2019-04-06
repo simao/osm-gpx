32d4d40ef1ccd67cc29845c260bb30b62ea3bb1a
Simão Mata <sm@0io.eu>
2019-04-06 11:53:02 +0200

    kind of working with pbf

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-extern crate quick_xml;
+extern crate osmpbfreader;
 extern crate geo_types;
 extern crate gpx;
 
 use std::fs::File;
-use quick_xml::events::attributes::Attributes;
 use std::str;
 
 use gpx::write;
@@ -12,48 +11,6 @@ use gpx::GpxVersion;
 use gpx::Waypoint;
 use geo_types::Point;
 
-use quick_xml::Reader;
-use quick_xml::events::Event;
-
-fn has_attribute(attrs: &Attributes, name: &str, value: &str) -> bool {
-    for att in attrs.clone() {
-        let att = att.unwrap();
-        if str::from_utf8(att.key).unwrap() == name && str::from_utf8(&att.value).unwrap() == value {
-            return true
-        }
-    };
-
-    false
-}
-
-fn extract_attribute(attrs: &Attributes, name: &str) -> Option<String> {
-    for att in attrs.clone() {
-        let att = att.unwrap();
-        if str::from_utf8(att.key).unwrap() == name {
-            return Some(String::from_utf8(att.value.to_vec()).unwrap())
-        }
-    };
-
-    None
-}
-
-fn extract_coordinates(attrs: &Attributes) -> Option<(f64, f64)> {
-    let mut lat = None;
-    let mut lon = None;
-
-    for att in attrs.clone() {
-        let att = att.unwrap();
-
-        if str::from_utf8(att.key).unwrap() == "lat" {
-            lat = Some(String::from_utf8(att.value.to_vec()).unwrap())
-        } else if str::from_utf8(att.key).unwrap() == "lon" {
-            lon = Some(String::from_utf8(att.value.to_vec()).unwrap())
-        }
-    }
-
-    lat.and_then(|a| lon.map(|b| (a.parse::<f64>().unwrap(), b.parse::<f64>().unwrap())))
-}
-
 fn write_gpx_data(data: Gpx) -> std::io::Result<()> {
     let buffer = File::create("foo.xml")?;
     write(&data, buffer).unwrap();
@@ -61,54 +18,27 @@ fn write_gpx_data(data: Gpx) -> std::io::Result<()> {
 }
 
 fn main() {
-    let mut reader = Reader::from_file("sachsen-anhalt-latest.osm").unwrap();
-    // let mut reader = Reader::from_file("test.xml").unwrap();
-    let mut buf = Vec::new();
-
-    let mut is_campsite = false;
-    let mut coordinates = None;
-    let mut campsite_name = None;
+    let filename = "sachsen-latest.osm.pbf";
+    let r = std::fs::File::open(&std::path::Path::new(filename)).unwrap();
+    let mut pbf = osmpbfreader::OsmPbfReader::new(r);
 
     let mut data : Gpx = Default::default();
     data.version = GpxVersion::Gpx11;
     data.waypoints = vec![];
 
-    loop {
-        match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) if e.name() == b"node" => {
-                coordinates = extract_coordinates(&mut e.attributes());
-            }
-            Ok(Event::Empty(ref e)) if e.name() == b"tag" => {
-                let attributes = e.attributes();
+    for obj in pbf.par_iter().map(Result::unwrap) {
+        if obj.tags().contains("tourism", "camp_site") {
+            let maybe_point = obj.node().map(|c| Point::new(c.lat(), c.lon()));
 
-                if has_attribute(&attributes, "k", "tourism") && has_attribute(&attributes, "v", "camp_site") {
-                    is_campsite = true;
-                } else if has_attribute(& attributes, "k", "name") {
-                    campsite_name = extract_attribute(&attributes, "v");
-                }
+            if let Some(point) = maybe_point {
+                let mut wpt = Waypoint::new(point);
+                wpt.name = obj.tags().get("name").map(|c| c.to_owned() );
+                println!("Found campsite named {:?} at {:?}", wpt.name, point);
+                data.waypoints.push(wpt);
+            } else {
+                println!("Could not get coordinates for {:?}", obj);
             }
-            Ok(Event::End(ref e)) if e.name() == b"node" => {
-                if is_campsite {
-                    println!("Found campsite named {:?} at {:?}", campsite_name, coordinates);
-                    let point = coordinates.map(|c| Point::new(c.1, c.0) ).unwrap();
-                    let mut wpt = Waypoint::new(point);
-                    wpt.name = campsite_name;
-                    data.waypoints.push(wpt);
-                }
-
-                coordinates = None;
-                is_campsite = false;
-                campsite_name = None;
-            }
-            Ok(Event::Eof) => break, // exits the loop when reaching end of file
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-            _ => {}
         }
-
-        buf.clear();
     }
 
     write_gpx_data(data).unwrap()
